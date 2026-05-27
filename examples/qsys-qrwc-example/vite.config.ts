4f0e04c66eb0c2767438d08ad956adcf1762a56c
Nick Paton <nickpaton@m3techgroup.com>
2026-05-27 12:20:39 -0400

    added unit test config

--- examples/qsys-qrwc-example/vite.config.ts
+++ examples/qsys-qrwc-example/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelteTesting } from '@testing-library/svelte/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
+  plugins: [tailwindcss(), svelte(), svelteTesting()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.ts'],
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
 })
